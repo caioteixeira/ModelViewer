df3998ccd6c1efec1469d9e6e4851eecfd1085bd
Caio Teixeira <caiovmteixeira@outlook.com>
2019-06-02 23:39:44 -0300

    Add file dialog lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <portable-file-dialogs.h>
+
 
 int main(int argc, char **argv)
 {
-    printf("Potato");
+    auto f = pfd::open_file("Choose files to read", "/tmp/",
+        { "Text Files (.txt .text)", "*.txt *.text",
+          "All Files", "*" },
+        true);
+    std::cout << "Selected files:";
+    for (auto const &name : f.result())
+        std::cout << " " + name;
+    std::cout << "\n";
 }
